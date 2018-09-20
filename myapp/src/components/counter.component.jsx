e9a7688c326a0e41f320f804d28c8618e469c0d9
Yatin Gupta <yatin.gupta@OPTIMUSDOM.COM>
2018-09-20 18:11:20 +0530

    click state is managed

--- myapp/src/components/counter.component.jsx
+++ myapp/src/components/counter.component.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 class CounterComponent extends Component {
   state = {
-    count: 1,
-    imageUrl: "http://picsum.photos/200"
+    count: 0,
+    imageUrl: "http://picsum.photos/200",
+    tags: []
   };
 
   styles = {
@@ -11,15 +12,42 @@ class CounterComponent extends Component {
     fontWeight: "bold"
   };
 
+  //   constructor() {
+  //     super(); // must call
+  //     this.incrementHandler = this.incrementHandler.bind(this); // this have reference of current class object,
+  //     // so binding this object with incrementHandler, always result in calling of
+  //     // same this.
+  //   }
+
+  renderTags() {
+    // helper method for conditional rendering tags
+    if (this.state.tags.length == 0) return <p>There are no tags!!</p>;
+    else
+      return (
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+  }
+
   render() {
     return (
       <React.Fragment>
         <span style={this.styles} className={this.getBadgeClassLabel()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm" styles={{ width: "10px" }}>
+        <button
+          className="btn btn-secondary btn-sm"
+          styles={{ width: "10px" }}
+          onClick={() => this.incrementHandler({ id: 1 })} // this syntax is used to pass parameter to click event handler
+          //onClick={this.incrementHandler}
+        >
           Increment
         </button>
+        {this.state.tags.length === 0 && "Please create a tag first"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
@@ -36,9 +64,22 @@ class CounterComponent extends Component {
     return count === 0 ? "Zero" : count;
   }
 
-  incrementCount() {
-    ++this.state.count;
-  }
+  //   incrementHandler() {
+  //     // this will not be defined here by default.
+  //     // To prevent this we need to bind(object=this) with this in this function
+  //     // Best place to do this is inside constructor
+  //     ++this.state.count;
+  //     console.log("Increment count clicked with count: " + this.state.count);
+  //     // same problem can be solved by using constructor function
+  //   }
+
+  incrementHandler = ob => {
+    //++this.state.count; // this will update count, but not reflect to view
+    // To reflect changes to view, it can be done as:
+    console.log(ob);
+    this.setState({ count: this.state.count + 1 });
+    console.log("Increment count clicked with count: " + this.state.count);
+  };
 }
 
 export default CounterComponent;

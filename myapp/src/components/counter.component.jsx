ca1e911b379b7a42d562ee0633c9cdf4863a660b
Yatin Gupta <yatin.gupta@OPTIMUSDOM.COM>
2018-09-20 16:48:43 +0530

    counter component added

--- myapp/src/components/counter.component.jsx
+++ myapp/src/components/counter.component.jsx
@@ -2,21 +2,38 @@ import React, { Component } from "react";
 
 class CounterComponent extends Component {
   state = {
-    count: 0
+    count: 1,
+    imageUrl: "http://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className={this.getBadgeClassLabel()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm" styles={{ width: "10px" }}>
+          Increment
+        </button>
       </React.Fragment>
     );
   }
 
+  getBadgeClassLabel() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    //return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 
   incrementCount() {
